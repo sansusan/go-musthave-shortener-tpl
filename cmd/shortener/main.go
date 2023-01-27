98efe0a36d48bb3042601a5ea47de5da915d8fe7
sansusan <sansusan@yandex.ru>
2023-01-28 01:34:57 +0300

    iter1

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,3 +1,51 @@
 package main
 
-func main() {}
+import (
+	"fmt"
+	"github.com/julienschmidt/httprouter"
+	"github.com/oklog/ulid/v2"
+	"io"
+	"log"
+	"net/http"
+	"sync"
+)
+
+var store = make(map[string]string)
+var mutex = sync.RWMutex{}
+
+func main() {
+	StartService()
+}
+
+func StartService() {
+	router := httprouter.New()
+	router.POST("/", ShortUrl)
+	router.GET("/:id", GetUrlById)
+
+	fmt.Printf("Starting server\n")
+	log.Fatal(http.ListenAndServe(":8080", router))
+}
+
+func ShortUrl(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	defer r.Body.Close()
+	url, _ := io.ReadAll(r.Body)
+	mutex.Lock()
+	defer mutex.Unlock()
+	var uid = ulid.Make().String()
+	store[uid] = string(url)
+	w.WriteHeader(201)
+	fmt.Fprintf(w, uid)
+}
+
+func GetUrlById(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	mutex.RLock()
+	defer mutex.RUnlock()
+	var url, exist = store[(ps.ByName("id"))]
+	if exist {
+		w.Header().Set("Location", url)
+		w.WriteHeader(307)
+		fmt.Fprintf(w, url)
+	} else {
+		w.WriteHeader(404)
+	}
+}
